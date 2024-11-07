6705fc053c9705c1505bafef20dbdecaded95a59
@hexlet/cli <support@hexlet.io>
2024-11-07 22:15:23 +0300

    submit java-spring-ru/annotations

--- java-spring-ru/annotations/src/main/java/exercise/annotation/Inspect.java
+++ java-spring-ru/annotations/src/main/java/exercise/annotation/Inspect.java
@@ -1,7 +1,6 @@
 package exercise.annotation;
 
 // BEGIN
-import javax.swing.text.Element;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
