6779787408a6d71a93144e6658e775ec3c58432a
@hexlet/cli <support@hexlet.io>
2024-12-29 00:49:04 +0300

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -11,23 +11,25 @@ public class Validator {
     public static List<String> validate(Object object) throws IllegalArgumentException, IllegalAccessException {
         var emptyFields = new ArrayList<String>();
         var fields = object.getClass().getDeclaredFields();
-            for (Field field : fields) {
-                if (field.isAnnotationPresent(NotNull.class)) {
-                    field.setAccessible(true);
-                    try {
-                        if (field.get(object) == null) {
-                            emptyFields.add(field.getName());
-                        }
-                    } catch (IllegalAccessException | IllegalArgumentException e) {
-                        e.printStackTrace();
+
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(NotNull.class)) {
+                field.setAccessible(true);
+                try {
+                    if (field.get(object) == null) {
+                        emptyFields.add(field.getName());
                     }
+                } catch (IllegalAccessException | IllegalArgumentException e) {
+                    e.printStackTrace();
                 }
             }
+        }
 
         return emptyFields;
     }
 
-    public static Map<String, List<String>> advancedValidate(Object object) throws IllegalArgumentException, IllegalAccessException {
+    public static Map<String, List<String>> advancedValidate(Object object)
+            throws IllegalArgumentException, IllegalAccessException {
         Map<String, List<String>> invalidFields = new HashMap<>();
         var fields = object.getClass().getDeclaredFields();
 
@@ -56,6 +58,7 @@ public class Validator {
                 e.printStackTrace();
             }
         }
+
         return invalidFields;
     }
 }
